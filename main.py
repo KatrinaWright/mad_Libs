2526cd2b8dd005bd1c9630f6c87cf0bb06827482
KatrinaWright <77311100+KatrinaWright@users.noreply.github.com>
2023-05-25 12:11:57 -0400

    Update main.py
    insert program

--- main.py
+++ main.py
@@ -1,1 +1,63 @@
+# Creating a mad lib program
+
+
+# Create containers for holding user inputs
+alan = "a/an"
+adjective1 = "one"
+part_body = "two"
+noun1 = "three"
+plural_noun = "four"
+male_name = "five"
+noun_place = "six"
+adverb = "seven"
+adjective2 = "eight"
+adjective3 = "nine"
+adjective4 = "ten"
+noun2 = "eleven"
+noun3 = "twelve"
+noun4 = "thirteen"
+noun5 = "fourteen"
+
+# function for deciding a versus an
+
+
+
+
+# list storys with input spots
+cinderella_story = f"""
+There once was {alan} {adjective1} young girl named Cinderella who lived with her {adjective2} stepmother and two {adjective3} stepsisters.
+She waited on them hand and {part_body}, but they treated her like {alan} {noun1}.
+Cinderella heard about a ball the prince was throwing, but she didn't have {alan} {adjective4} gown to wear.
+Then, out of the clear, blue {noun2}, her fairy {noun3}-mother appeared and waved her magic {noun4}.
+Cinderella's ragged clothes turned into a beautiful {noun5}, and her worn work shoes became a pair of glass {plural_noun}.
+Cinderella went to the ball and danced with Prince {male_name}, who fell madly in love with her.
+But at the stroke of midnight she had to flee, losing one of her glass {plural_noun}.
+The prince travelled throughout the kingdom, trying to find the matching {plural_noun}.
+He visited the {noun_place} of every young girl until he found Cinderella, who had the other half of the {plural_noun}.
+Prince {male_name} and Cinderella soon were married and lived {adverb} ever after!  
+"""
+
+
+#user inputs strings into containers
+
+
+# Quality check user input 
+
+
+
+
+# Create function that inserts containers into story
+
+
+
+
+
+# randomize the story that is given when opening the app
+
+print(cinderella_story)
+
+
+# ask user if they would like to do another story
+
+
 
